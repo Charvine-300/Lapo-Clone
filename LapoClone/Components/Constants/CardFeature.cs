8e44930ad0f4e0a495d3454447290877105cfc1d
Charvine-300 <marvinezenwekwe@gmail.com>
2025-01-26 21:26:16 +0100

    Blazor app init

---
+++ LapoClone/Components/Constants/CardFeature.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LapoClone.Components.Constants
+{
+    public class CardFeature
+    {
+        public required string Icon { get; set; }
+        public required string Feature { get; set; }
+        public required string Url { get; set; }
+    }
+}
---
+++ LapoClone/Components/Constants/NavbarItems.cs
@@ -0,0 +1,9 @@
+﻿namespace LapoClone.Components.Constants
+{
+    public class NavbarItems
+    {
+        public string Text { get; set; }
+        public string Url { get; set; }
+        public string? Icon { get; set; } = null;
+    }
+}
